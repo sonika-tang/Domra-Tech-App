2a215629d7e9ddd60dc5362bb6fecdec18f73364
OengGechty <gechtyoeng@gmail.com>
2026-02-02 21:52:40 +0700

    fix/motify app to use configure theme data

--- lib/core/config/theme.dart
+++ lib/core/config/theme.dart
@@ -5,17 +5,48 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: AppColors.primary, 
-      background: AppColors.background
-      ),
-    
+
+    //configure color theme
+    colorScheme: const ColorScheme.light(
+      primary: AppColors.primary,
+      onPrimary: AppColors.background,
+
+      secondary: AppColors.secondary,
+      onSecondary: AppColors.background,
+
+      background: AppColors.background,
+      onBackground: AppColors.textPrimary,
+
+      primaryContainer: AppColors.primaryBackground,
+      onPrimaryContainer: AppColors.gray,
+
+      secondaryContainer: AppColors.secondaryBackground,
+      onSecondaryContainer: AppColors.secondaryHover,
+
+      tertiaryContainer: AppColors.lightGray,
+      onTertiaryContainer: AppColors.gray,
+
+      error: AppColors.error,
+      onError: Colors.white,
+
+    ),
+
+    //configure text theme
+    textTheme: const TextTheme(
+      displayLarge: AppTextStyle.largeTitle,
+      headlineLarge: AppTextStyle.heading1,
+      headlineMedium: AppTextStyle.heading2,
+      headlineSmall: AppTextStyle.heading3,
+
+      bodyLarge: AppTextStyle.body1,
+      bodyMedium: AppTextStyle.body2,
+      bodySmall: AppTextStyle.small,
+    ),
+
+    //config scaffold default color
     scaffoldBackgroundColor: AppColors.background,
 
-    appBarTheme: const AppBarTheme(
-      backgroundColor: AppColors.background,
-      elevation: 0,
-      titleTextStyle: AppTextStyle.heading3,
-    )
+    //config app bar default color
+    appBarTheme: const AppBarTheme(backgroundColor: AppColors.primary, elevation: 0, titleTextStyle: AppTextStyle.heading3),
   );
 }
