6ebf21af22e54ef203df4ead711ca276403ed266
vesyean <veyseankem@gmail.com>
2026-03-08 13:03:44 +0700

    onboarding screen 1-3

--- lib/ui/screens/onboarding/onboarding_3_screen.dart
+++ lib/ui/screens/onboarding/onboarding_3_screen.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import '../../../routes/app_routes.dart';
+import '../../widgets/actions/primary_button.dart';
+import 'widgets/onboarding_content.dart';
 
 class OnboardingScreen3 extends StatelessWidget {
   const OnboardingScreen3({super.key});
+
   @override
-  Widget build(BuildContext context) =>
-      Scaffold(body: Center(child: Text('Onboarding 1')));
+  Widget build(BuildContext context) {
+    return OnboardingPage(
+      title: "Contribute To Khmer Technical Vocabulary",
+      subtitle:
+          "Add new terms or request corrections to improve the community.",
+      illustration: Image.asset("assets/imgs/request2.png", height: 200),
+      currentPage: 3,
+
+      bottomWidget: PrimaryButton(
+        label: "Get Start",
+        onPressed: () {
+          Navigator.pushReplacementNamed(context, AppRoutes.welcome);
+        },
+      ),
+    );
+  }
 }
