ab150cb9029856971d5c8211a4ffe7dcf6dfa79c
sonika-tang <sonikatang630@gmail.com>
2026-03-08 18:04:13 +0700

    Update folder path

--- lib/ui/screens/authentication/login_screen.dart
+++ lib/ui/screens/authentication/login_screen.dart
@@ -1,6 +1,6 @@
+import 'package:domra_tech/state/provider/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../../../providers/auth_provider.dart';
 import 'widgets/login_form.dart';
 
 class LoginScreen extends StatefulWidget {
